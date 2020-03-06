205bdc3347e58066b36a5e5952ae73fd6b7e87c2
Ludde86 <bslbjorn@gmail.com>
2020-03-06 10:43:39 +0100

    fix UserItem as a functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-	// state = {
-	// 	id: 'id',
-	// 	login: 'mojombo',
-	// 	avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-	// 	html_url: 'https://github.com/mojombo'
-	// };
-	render() {
-		const { avatar_url, login, html_url } = this.props.user; // pulling data out of this object
-		return (
-			<div className="card text-center">
-				<img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
-				<h3>{login}</h3>
-				<div>
-					<a href={html_url} className="btn btn-dark btn-sm my-1">
-						{html_url}
-					</a>
-				</div>
+const UserItem = (props) => {
+	const { avatar_url, login, html_url } = props.user; // pulling data out of this object
+	return (
+		<div className="card text-center">
+			<img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+			<h3>{login}</h3>
+			<div>
+				<a href={html_url} className="btn btn-dark btn-sm my-1">
+					{html_url}
+				</a>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default UserItem;
