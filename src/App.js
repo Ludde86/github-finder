2cb1cc9d1d4804532a059f20c4ac5a358ed705c0
Ludde86 <bslbjorn@gmail.com>
2020-03-06 20:14:59 +0100

    add fetch search data

--- src/App.js
+++ src/App.js
@@ -10,31 +10,44 @@ class App extends Component {
 		users: [],
 		loading: false
 	};
-	async componentDidMount() {
-		this.setState({ loading: true });
-		// Get-request to the github api
-		// axios deals with promises (then -> response or reject) -> gets a response, as data
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+	// 	// Get-request to the github api
+	// 	// axios deals with promises (then -> response or reject) -> gets a response, as data
 
-		// axios.get('https://api.github.com/users').then((res) => console.log(res.data));
-		// refactored -> await a response or reject
+	// 	// axios.get('https://api.github.com/users').then((res) => console.log(res.data));
+	// 	// refactored -> await a response or reject
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=$
+	// 		{process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+	// 		{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+
+	// 	this.setState({
+	// 		users: res.data,
+	// 		loading: false
+	// 	});
+	// }
+
+	searchUsers = async (text) => {
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=$
+			`https://api.github.com/search/users?q=${text}&client_id=$
 			{process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
 			{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
-
+		console.log(res.data.items);
 		this.setState({
-			users: res.data,
+			users: res.data.items,
 			loading: false
 		});
-	}
+	};
 
 	render() {
 		return (
 			<div className="App">
 				<Navbar title="Github Finder" icon="fab fa-github" />
 				<div className="container">
-					<Search />
+					<Search searchUsers={this.searchUsers} />
 					<Users users={this.state.users} loading={this.state.loading} />
 				</div>
 			</div>
