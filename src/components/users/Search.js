3ec3459f0905aa3393bafca289d8b76c1e7e22cf
Ludde86 <bslbjorn@gmail.com>
2020-03-06 15:34:10 +0100

    add on change event for input

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+	state = {
+		text: ''
+	};
 	render() {
 		return (
 			<div>
 				<form className="form">
-					<input type="text" name="text" placeholder="Search Users..." />
+					<input
+						type="text"
+						name="text"
+						placeholder="Search Users..."
+						value={this.state.text}
+						onChange={(e) => this.setState({ text: e.target.value })}
+						autoComplete="off"
+					/>
 					<input type="submit" value="search" className="btn btn-dark btn-block" />
 				</form>
 			</div>
