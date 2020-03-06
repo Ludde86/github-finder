5b4c42e8c7f72cc5d315d34f5b5f30ff3e3053c3
Ludde86 <bslbjorn@gmail.com>
2020-03-06 10:31:53 +0100

    add styled object for each UserItem

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -25,8 +25,14 @@ class Users extends Component {
 		]
 	};
 	render() {
-		return <div>{this.state.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
+		return <div style={userStyle}>{this.state.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
 	}
 }
+
+const userStyle = {
+	display: 'grid', // this element is using grid
+	gridTemplateColumns: 'repeat(3, 1fr)', // places elements in three columns, all with the same size
+	gridGap: '1rem' // sets a gap between all elements
+};
 
 export default Users;
