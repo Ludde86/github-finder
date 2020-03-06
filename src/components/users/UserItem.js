697903aa8776695e2a49a56bc3b7684fd55d96ce
Ludde86 <bslbjorn@gmail.com>
2020-03-06 10:53:03 +0100

    fix Navbar as functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const UserItem = (props) => {
-	const { avatar_url, login, html_url } = props.user; // pulling data out of this object
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
 	return (
 		<div className="card text-center">
 			<img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
