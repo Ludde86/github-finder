1121aef2008a93a6f79eb642817ae2d57570ed18
Ludde86 <bslbjorn@gmail.com>
2020-03-09 14:26:12 +0100

    fix Link instead of a tags

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
 	return (
@@ -9,10 +10,10 @@ const Navbar = ({ icon, title }) => {
 			</h1>
 			<ul>
 				<li>
-					<a href="/">Home</a>
+					<Link to="/">Home</Link>
 				</li>
 				<li>
-					<a href="/about">About</a>
+					<Link to="/about">About</Link>
 				</li>
 			</ul>
 		</nav>
