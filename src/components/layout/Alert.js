465b0c8ce36d909bc055db55dc9dd7474b61a469
Ludde86 <bslbjorn@gmail.com>
2020-03-09 13:37:47 +0100

    fix destrucuring props in Alert.js

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Alert = (props) => {
+const Alert = ({ alert }) => {
 	return (
-		props.alert !== null && (
+		alert !== null && (
 			// here we display the props sent from the alert state
-			<div className={`alert alert-${props.alert.type}`}>
-				<i className="fas fa-info-circle" /> {props.alert.msg}
+			<div className={`alert alert-${alert.type}`}>
+				<i className="fas fa-info-circle" /> {alert.msg}
 			</div>
 		)
 	);
