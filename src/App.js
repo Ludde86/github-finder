c51d91e32b5dbcd9a3d9023daaf9c8957144dd55
Ludde86 <bslbjorn@gmail.com>
2020-03-10 11:33:58 +0100

    add function and state for repos

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 	state = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 		alert: null
 	};
@@ -58,6 +59,21 @@ class App extends Component {
 			{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 		this.setState({ user: res.data, loading: false });
+	};
+
+	getUserRepos = async (username) => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+			{process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+			{process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({
+			repos: res.data,
+			loading: false
+		});
 	};
 
 	clearUsers = () => {
