920827428b9e2ac2f976d9899c0877bd886c0003
Ludde86 <bslbjorn@gmail.com>
2020-03-10 10:05:35 +0100

    fix pull out all data we want from the user

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 
 class User extends Component {
+	// pull the username (the login) from the url, and then call getUser
+	// params = '/user/:login'
+	// this params is passed in as an argument (username)
+	// -> fetch the data with this username
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.login);
+	}
 	render() {
+		const {
+			name,
+			avatar_url,
+			location,
+			bio,
+			blog,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+
 		return (
 			<div>
 				<h1>User</h1>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -8,7 +8,7 @@ const UserItem = ({ user: { avatar_url, login } }) => {
 			<img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
 			<h3>{login}</h3>
 			<div>
-				<Link to={`/users/${login}`} className="btn btn-dark btn-sm my-1">
+				<Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
 					More
 				</Link>
 			</div>
